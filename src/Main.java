6e3c46e77efc174b252711361485ee2438ef3096
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-12 10:05:23 -0400

    27. Remove Element https://leetcode.com/problems/remove-element/submissions/2030876007/?envType=problem-list-v2&envId=dphj3843&

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,19 @@
 
 // LeetCode
     void main() {
-    
+    int[] nums = {3,2,2,3};
+    System.out.println(removeElement(nums, 3));
 }
 
     public int removeElement(int[] nums, int val) {
-
-        return val;
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
     }
 
 
