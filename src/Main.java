103f34412663200cd2fdaf7ee1468dfa18f1a883
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-14 00:23:24 -0400

    35. Search Insert Position https://leetcode.com/problems/search-insert-position/description/?envType=problem-list-v2&envId=dphj3843

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
 
 // LeetCode
     void main() {
-    String s1 = "hello";
-    String s2 = "ll";
-    System.out.println(strStr(s1,s2));
-    
+    int[] nums = {1,3,5,6};
+    System.out.println(searchInsert(nums, 5));
 }
 
-    public int strStr(String haystack, String needle) {
-        int h = haystack.length();
-        int n = needle.length();
+    public int searchInsert(int[] nums, int target) {
+        int l = 0;
+        int r = nums.length;        
 
-        for (int i = 0; i <= h - n; i++) {
-            if (haystack.substring(i, i + n).equals(needle)) {
-                return i;
+        while (l < r) {          
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] >= target) {  
+                r = mid;
+            } else {
+                l = mid + 1;
             }
         }
 
-        return -1;
+        return l;
     }
 
 
