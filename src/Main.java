db68bc7185f2be3b13d0319be8df793b0ba81de6
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-15 23:20:52 -0400

    58. Length of Last Word https://leetcode.com/problems/length-of-last-word/description/?envType=problem-list-v2&envId=dphj3843

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,19 @@
 }
     /// Given a string s consisting of words and spaces, return the length of the last word in the string.
     public int lengthOfLastWord(String s) {
+        int i = s.length() - 1;
+        int count = 0;
+        
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+        
+        while (i >= 0 && s.charAt(i) != ' ') {
+            count++;
+            i--;
+        }
 
-        return 0;
+        return count;
     }
 
 
