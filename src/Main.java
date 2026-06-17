bf275ce687c0a32149bbd1d3810d41d67badeae3
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-16 22:51:38 -0400

    66. Plus One https://leetcode.com/problems/plus-one/description/?envType=problem-list-v2&envId=dphj3843

--- src/Main.java
+++ src/Main.java
@@ -3,21 +3,20 @@
     void main() {
     
 }
-    /// Given a string s consisting of words and spaces, return the length of the last word in the string.
-    public int lengthOfLastWord(String s) {
-        int i = s.length() - 1;
-        int count = 0;
-        
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
-        
-        while (i >= 0 && s.charAt(i) != ' ') {
-            count++;
-            i--;
+    ///You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;  // no carry, done immediately
+            }
+            digits[i] = 0;      // was 9, becomes 0, carry continues
         }
 
-        return count;
+        // Only reach here if every digit was 9 (e.g. [9,9,9])
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;          // [1, 0, 0, 0]
+        return result;
     }
 
 
