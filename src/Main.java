9e8f2269ccce829b7ca2c18099d2c7d4c1603f72
MonkeyBoi <monkeylaguri308@gmail.com>
2026-06-20 23:26:32 -0400

    69. Sqrt(x) https://leetcode.com/problems/sqrtx/description/?envType=problem-list-v2&envId=dphj3843

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,23 @@
 }
     /// Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
     public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int result = 0;
 
-        return x;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if ((long) mid * mid == x) {
+                return mid;
+            } else if ((long) mid * mid < x) {
+                result = mid;        
+                left = mid + 1;      
+            } else {
+                right = mid - 1;     
+            }
+        }
+
+        return result;
     }
-
 
